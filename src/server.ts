79198708a72874c48c1fcf161819e61f3f4fb24f
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-01-04 17:23:56 -0300

    Diagrama em svg e pacote para cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import cors from '@fastify/cors';
 
 const prisma = new PrismaClient({
 	log: ['query'],
@@ -8,6 +9,10 @@ const prisma = new PrismaClient({
 async function bootstrap() {
 	const fastify = Fastify({
 		logger: true,
+	})
+
+	await fastify.register(cors, {
+		origin: true
 	})
 
 	fastify.get('/pools/count', async () => {
@@ -22,7 +27,7 @@ async function bootstrap() {
 		return { pools };
 	});
 
-	await fastify.listen({ port: 3333 });
+	await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
