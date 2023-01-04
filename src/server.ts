bca7519755aa6b1468aa45905222405cf23208f5
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-01-04 10:00:37 -0300

    Encontrando os bolões

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+	log: ['query'],
+});
 
 async function bootstrap() {
 	const fastify = Fastify({
 		logger: true,
 	})
 
-	fastify.get('/pools/count', () => {
-		return { count: 121 };
+	fastify.get('/pools/count', async () => {
+		const pools = await prisma.pool.findMany({
+			where: {
+				code: {
+					startsWith: 'i'
+				}
+			}
+		})
+
+		return { pools };
 	});
 
 	await fastify.listen({ port: 3333 });
